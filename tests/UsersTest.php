73e66d1d4b12b8d1da4cbae732bb42b5bbde3735
Javier C <javiercicchino@gmail.com>
2016-09-17 19:02:43 -0300

    Agrego un test para registrar y loguear usuarios

---
+++ tests/UsersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRegisterUsers()
+    {
+        /*
+        $this->assertTrue(true);
+        */
+        $this->visit('/register')
+            ->type('Javier','nombre')
+            ->type('Cicchino','apellido')
+            ->type('28869140','dni')
+            ->select('administrador','tipo')
+            ->type('javiercicchino@gmail.com','email')
+            ->type('asdfg','password')
+            //->type('asdfgh','password-confirm')
+            ->press('Registrarse');
+
+    }
+
+    public function testLoginUser() {
+
+        $this->visit('/login')
+            ->type('ja@ho.com','email')
+            ->type('123456','password')
+            ->press('Login')
+            ->see('Laravel');
+
+    }
+}
