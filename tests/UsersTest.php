9fdf38bdd9fabfd41679f01f0d5c598a24806a78
Javier C <javiercicchino@gmail.com>
2016-09-21 18:30:31 -0300

    Modifica UsersTest

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -6,45 +6,43 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UsersTest extends TestCase
 {
-
     use DatabaseMigrations;
     /**
      * A basic test example.
      *
      * @return void
+     *
+     *
      */
 
-
-
-
+    protected $nombre = 'Javier';
+    protected $apellido = 'Cicchino';
+    protected $dni = '33';
+    protected $tipo = 'Administrador';
+    protected $email= 'javier@jajaja.com';
+    protected $password = '123456';
 
 
     public function testRegisterUsers()
     {
-        /*
-        $this->assertTrue(true);
-        */
         $this->visit('/register')
-            ->type('Javier','nombre')
-            ->type('Cicchineo','apellido')
-            ->type('28869130','dni')
-            ->select('administrador','tipo')
-            ->type('javiercicchino1@gmail.com','email')
-            ->type('asdfgh','password')
-            ->type('asdfgh','password_confirmation')
+            ->type($this->nombre,'nombre')
+            ->type($this->apellido,'apellido')
+            ->type($this->dni,'dni')
+            ->select($this->tipo,'tipo')
+            ->type($this->email,'email')
+            ->type($this->password,'password')
+            ->type($this->password,'password_confirmation')
             ->press('Registrarse')
             ->seePageIs('/home');
-            //->;
-
     }
 
-    public function testLoginUser() {
-
+    public function testLoginUser()
+    {
         $this->visit('/login')
-            ->type('javiercicchino1@gmail.com','email')
-            ->type('asdfgh','password')
+            ->type($this->email,'email')
+            ->type($this->password,'password')
             ->press('Login')
             ->see('You are logged in!');
-
     }
 }
