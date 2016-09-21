c6dd9f90a66673e0f5f247c9eecb40e77e950b61
Javier C <javiercicchino@gmail.com>
2016-09-21 20:04:33 -0300

    Se modifica UsersTest y se agregan comentarios

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UsersTest extends TestCase
 {
+    // Con esta opcion se hace migracion cada vez que se ejecuta un test
     use DatabaseMigrations;
     /**
      * A basic test example.
@@ -15,14 +16,15 @@ class UsersTest extends TestCase
      *
      */
 
+    // Se definen las variables que contienen los datos para probar usuarios
     protected $nombre = 'Javier';
     protected $apellido = 'Cicchino';
-    protected $dni = '33';
-    protected $tipo = 'Administrador';
+    protected $dni = '28869140';
+    protected $tipo = 'administrador';
     protected $email= 'javier@jajaja.com';
     protected $password = '123456';
 
-
+    // Funcion que prueba el registro de un usuario nuevo
     public function testRegisterUsers()
     {
         $this->visit('/register')
@@ -34,15 +36,30 @@ class UsersTest extends TestCase
             ->type($this->password,'password')
             ->type($this->password,'password_confirmation')
             ->press('Registrarse')
-            ->seePageIs('/home');
+            ->seePageIs('/home')
+            //->visit('/home#')
+            //->visit('/logout')
+            //->click('navbar-toogle')
+            //->press('dropdown')
+            //->click('Logout')
+            //->visit('/login')
+            //->type($this->email,'email')
+            //->type($this->password,'password')
+            //->press('Login')
+            ->see('You are logged in!');
     }
 
+    //Funcion que prueba el login de un usuario
+    /*
     public function testLoginUser()
     {
         $this->visit('/login')
             ->type($this->email,'email')
             ->type($this->password,'password')
+            //->type('jas@ho.com','email')
+            //->type('123456','password')
             ->press('Login')
             ->see('You are logged in!');
     }
+*/
 }
