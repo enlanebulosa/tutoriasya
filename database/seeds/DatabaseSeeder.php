393a4b17f82909bba9f6d866ebe7f160cb7c453d
Matias Mendoza <matiasmendoza1994@gmail.com>
2016-09-23 17:01:38 -0300

    Nuevo archivo para seedear la tabla users la cual utiliza el orm User.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(usuariosrandomSeeder::class);
+        $this->call(dbseed::class);
+        
     }
 }
---
+++ database/seeds/dbseed.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Faker\Factory as Faker;
+
+class dbseed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$faker=Faker::create();
+    	for($i=0; $i<25; $i++){
+    		$user = User::create(array(
+    			'nombre' =>$faker-> firstNameMale,
+    			'apellido' =>$faker-> word,
+    			'dni'=> rand(0000000, 99999999),
+    			'email'=> $faker-> email,
+    			'tipo'=>$faker-> randomElement (['profesor','administrador', 'alumno']),
+    			'password'=>bcrypt('secret'),
+    		));
+                $user->save();
+    	}
+       
+    }
+}
