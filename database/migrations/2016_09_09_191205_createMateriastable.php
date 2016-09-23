a1b975112ad054a4c9a160c685b0817ff719bc23
Matias Mendoza <matiasmendoza1994@gmail.com>
2016-09-23 12:42:09 -0300

    Se agrega la tabla migraciones

---
+++ database/migrations/2016_09_09_191205_createMateriastable.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMateriastable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 45);
+            $table->string('nivel');
+            $table->timestamps();
+        });
+        
+        Schema::create('user_materia', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_usuario')->unsigned();
+            $table->integer('id_materia')->unsigned(); 
+            
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->foreign('id_materia')->references('id')->on('materias'); 
+           
+            $table->timestamps();
+            
+       });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('materias');
+    }
+}
