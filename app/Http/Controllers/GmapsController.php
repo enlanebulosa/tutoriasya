40db2ce4a1193171d67720b34584a7dffe6e64d9
Jcantero95 <jcantero95@gmail.com>
2016-11-02 19:20:11 -0300

    Se agrega GmapsController

---
+++ app/Http/Controllers/GmapsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GmapsController extends Controller
+{
+    public function index(){
+    	$config = array() ;
+        $config['center'] = 'auto';
+        $config['map_width'] = 400;
+        $config['map_height'] = 400;
+        $config['zoom'] = 15;
+        $config['onboundschanged'] = 'if (!centreGot) {
+            var mapCentre = map.getCenter();
+            marker_0.setOptions({
+                position: new google.maps.LatLng(mapCentre.lat(), mapCentre.lng())
+ 
+            });
+        }
+        centreGot = true;';
+ 
+        \Gmaps::initialize($config);
+ 
+        // Colocar el marcador 
+        // Una vez se conozca la posición del usuario
+        $marker = array();
+        \Gmaps::add_marker($marker);
+ 
+        $map = \Gmaps::create_map();
+ 
+        //Devolver vista con datos del mapa
+        return view('gmaps', compact('map'));
+    }
+
+
+
+}
