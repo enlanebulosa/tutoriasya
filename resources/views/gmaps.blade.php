4b6ce70e75f85ac4b87029635ff5ea4d06bc5966
Jcantero95 <jcantero95@gmail.com>
2016-11-02 19:18:51 -0300

    Se agrega la vista del mapa

---
+++ resources/views/gmaps.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mapa de la aplicacion</title>
+	<script type="text/javascript">var centreGot = false;</script>{!!$map['js']!!}
+</head>
+<body>
+	<p>Mapa de la aplicacion</p>
+	{!!$map['html']!!}
+</body>
+</html>
