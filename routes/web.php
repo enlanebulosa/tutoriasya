423ac6d8b92561ad8f3c41ef5b324cfddac6e7f7
Jcantero95 <jcantero95@gmail.com>
2016-11-02 12:42:12 -0300

    Se agrega la ruta para funcionamiento del mapa

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 
 Auth::routes();
 
+Route::get('/gmaps', ['as' => 'gmaps', 'uses' => 'GmapsController@index']);
 Route::get('/home', 'HomeController@index');
 Route::get('/', 'RoutesController@checkAuth');
 
