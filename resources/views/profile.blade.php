6cce7ab5bf3422959c06a799c1bcc0875f583b00
Matias Mendoza <matiasmendoza1994@gmail.com>
2016-11-26 23:45:48 -0300

    Se agrega boton para poder consultar a un profesor en la vista de su perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
- 
+
 @section('content')
 
 
 
 
-@if (Auth::user()->tipo=="administrador") 
+@if (Auth::user()->tipo=="administrador")
 <body style="background-color:  #ba3b3b  ;">
 @elseif (Auth::user()->tipo=="alumno")
 
@@ -19,27 +19,21 @@
     <div class="row">
     <font color="white">
         <div class="col-md-10 col-md-offset-1">
-            <img src='/uploads/avatars/{{$user->avatar}}' style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;"> 
+            <img src='/uploads/avatars/{{$user->avatar}}' style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
             <h2>Perfil de {{$user->nombre}}</h2>
             {{$user->tipo}}<br/>
             @if (Auth::user()->id==$user->id)
-            <form enctype="multipart/form-data" action="/profile" method="POST">
-            <label>Cambiar el avatar</label>
-            <input type="file" name="avatar">
-            <input type="hidden" name="_token" value='{{ csrf_token() }}'>
-            <input type="submit" class="pull-right btn btn-sm btn-primary">
-            </form>
+              <form enctype="multipart/form-data" action="/profile" method="POST">
+                <label>Cambiar el avatar</label>
+                <input type="file" name="avatar">
+                <input type="hidden" name="_token" value='{{ csrf_token() }}'>
+                <input type="submit" class="pull-right btn btn-sm btn-primary">
+              </form>
             @else
-            <form enctype="multipart/form-data" action="/consulta" method="POST">
-            <label>Contactar</label>
-            
-            <input type="hidden" name="_token" value={{$user->id}}>
-            
-           <input type="submit" class="pull-right btn btn-sm btn-primary">
-            </form>
+              <a href="/consulta/{{$user->id}}"><button type="button" name="button">Contactar</button></a>
             @endif
 
-            
+
             </div>
         </div>
     </div>
@@ -54,7 +48,7 @@
          <div class="panel-heading">
         <center><font face="Comic Sans MS,arial,verdana">Historial</font></center>
         </div>
-        
+
         <div class="panel-body">
         {{$user->nombre}} se conectó por última vez en el {{$user->updated_at}} <br/>
         <br/>
