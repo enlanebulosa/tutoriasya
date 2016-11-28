59a23ecd43d43a1f41d8ea3d0fa3ddd2191f52e6
Jcantero95 <jcantero95@gmail.com>
2016-11-28 01:27:41 -0300

    Se borran lineas extras

--- resources/views/gmaps.blade.php
+++ resources/views/gmaps.blade.php
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<script type="text/javascript">var centreGot = false;</script>{!!$map['js']!!}
-</head>
-<body>
-	{!!$map['html']!!}
-</body>
-=======
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Mapa de la aplicacion</title>
-	<script type="text/javascript">var centreGot = false;</script>{!!$map['js']!!}
+	<script type="text/javascript">var centreGot = false;</script>
+	{!!$map['js']!!}
 </head>
 <body>
 	<p>Mapa de la aplicacion</p>
 	{!!$map['html']!!}
 </body>
->>>>>>> 51452fa77722526ec07276a6dc6242beea5f6770
 </html>
